a315e7c3f2ff727b2448278da82a381bbf7030be
Jang Yeongjun <150320634+Jang-Yeongjun@users.noreply.github.com>
2023-12-14 22:57:57 +0900

    Input Money
    입력을 통한 입금, 출금 구현

--- Assets/Script/ScreenChanger.cs
+++ Assets/Script/ScreenChanger.cs
@@ -11,7 +11,10 @@ public class ScreenChanger : MonoBehaviour
     public TextMeshProUGUI _secondText;
     public TextMeshProUGUI _thirdText;
     public TextMeshProUGUI _fourthText;
+    public TextMeshProUGUI _stateText;
+    public TMP_InputField _inputValue;
     public GameObject _button;
+    public GameObject _state;
     public float fontSize = 36f;
     ProcessData processData;
     private void Start()
@@ -26,35 +29,49 @@ public class ScreenChanger : MonoBehaviour
             if (buttonValue == 10000)
             {
                 _button.tag = "remittance";
+                _stateText.text = "송금";
             }
             else if (buttonValue == 50000)
             {
                 _button.tag = "deposit";
+                _stateText.text = "입금";
             }
             else if (buttonValue == 100000)
             {
                 _button.tag = "loan";
+                _stateText.text = "대출";
             }
             else if (buttonValue == 0)
             {
                 _button.tag = "withdrawal";
+                _stateText.text = "출금";
             }
         }
         else if(_button.CompareTag("deposit"))
         {
-            processData.DepositProcess(buttonValue);
+            if(buttonValue == 0)
+            {
+                processData.DepositProcess(buttonValue+ int.Parse(_inputValue.text));
+            }
+            else {processData.DepositProcess(buttonValue);}
+            
         }
         else if (_button.CompareTag("withdrawal"))
         {
+            if (buttonValue == 0)
+            {
+                processData.WithdrawalProcess(buttonValue + int.Parse(_inputValue.text));
+            }
             processData.WithdrawalProcess(buttonValue);
         }
     }
     public void ButtonText()
     {
+        _state.SetActive(true);
         _firstText.text = "10,000";
         _secondText.text = "50,000";
         _thirdText.text = "100,000";
-        _fourthText.text = "직접입력";
+        _fourthText.text = "입력 값";
         _firstText.fontSize = fontSize - 16f;
         _secondText.fontSize = fontSize - 16f;
         _thirdText.fontSize = fontSize - 16f;
@@ -63,6 +80,7 @@ public class ScreenChanger : MonoBehaviour
 
     public void BankMainScreen()
     {
+        _state.SetActive(false);
         _firstText.text = "송금";
         _secondText.text = "입금";
         _thirdText.text = "대출";
