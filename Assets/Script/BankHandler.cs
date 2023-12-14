f0338662f5c0d8a719c873e4c3ce756ace9d1343
Jang Yeongjun <150320634+Jang-Yeongjun@users.noreply.github.com>
2023-12-14 23:11:24 +0900

    name Change
    Name Change

--- Assets/Script/BankHandler.cs
+++ Assets/Script/BankHandler.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BankManager : MonoBehaviour
+public class BankHandler : MonoBehaviour
 {
     ScreenChanger screenChanger;
     private void Start()
